38fb752ccc673eb859553d8f44e439e94b9abcd8
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2025-01-19 18:26:21 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/qualcomm/msm8953_64
+DEVICE_PATH := device/Android/msm8953_64
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
