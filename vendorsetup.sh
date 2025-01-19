111ca385db17140ae057744c8b10ade9d8c59c8a
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2025-01-19 18:49:38 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+if [ -f "${MY_DIR}/prebuilt/kernel.zip" ]; then
+echo "Extracting kernel..."
+unzip -p "${MY_DIR}/prebuilt/kernel.zip" kernel > "${MY_DIR}/prebuilt/kernel"
+echo "Done."
+fi
 add_lunch_combo omni_msm8953_64-user
 add_lunch_combo omni_msm8953_64-userdebug
 add_lunch_combo omni_msm8953_64-eng
